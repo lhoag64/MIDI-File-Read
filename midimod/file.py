29fb83d867d8f072d1182fa1e26a7a590f7dcacc
lhoag <lhoag@yahoo.com>
2018-01-30 16:55:21 -0600

    Pre-move of file.py
    Commiting file.py before changing file to part of utilities.

--- midimod/file.py
+++ midimod/file.py
@@ -3,7 +3,88 @@ import os.path
 from utilities.hexdump import HexDump
 
 #----------------------------------------------------------------------
+# MHdr Class
+#---
+# Data related to a MHdr
+#----------------------------------------------------------------------
+class MHdr:
+  fmt = None
+  ntrks = None
+  division = None
+
+  #--------------------------------------------------------------------
+  def __init__(self, chunk):
+    logging.debug("Chunk|"+chunk.ctype)
+    HexDump("Chunk Data", chunk.data)
+    self.fmt = int.from_bytes(chunk.data[0:2], byteorder="big")
+    self.ntrks = int.from_bytes(chunk.data[2:4], byteorder="big")
+    self.division = int.from_bytes(chunk.data[4:6], byteorder="big")
+
+#----------------------------------------------------------------------
+class MTrkEvent:
+  deltaTime = None
+  event = None
+
+#----------------------------------------------------------------------
+class Sysex:
+  sLen = None
+  sData = None
+
+#----------------------------------------------------------------------
+class MetaEvent:
+  eType = None
+  eLen = None
+  eData = None
+
+#----------------------------------------------------------------------
+class MidiEvent:
+  sByte = None
+
+#----------------------------------------------------------------------
+# MTrk Class
+#----------------------------------------------------------------------
+class MTrk:
+  #--------------------------------------------------------------------
+  def __init__(self, chunk):
+    logging.debug("Chunk|"+chunk.ctype)
+    HexDump("Chunk Data", chunk.data[0:32])
+    clen = chunk.data.nbytes
+    cidx = 0
+    while cidx < clen:
+      #----------------------------------------------------------------
+      # Read Delta-Time
+      #----------------------------------------------------------------
+      idx = cidx
+      val = 0
+      while idx < 4:
+        buf = chunk.data[idx] & 0xff
+        idx += 1
+        val = (val << 7) + buf
+        if (buf & 0x80) == 0:
+          break
+      deltaTime = val
+      cidx += idx
+
+      #----------------------------------------------------------------
+      # Read type - Sysex (F0), Meta (FF), Midi
+      #----------------------------------------------------------------
+      buf = chunk.data[cidx] & 0xff
+      cidx += 1
+      if buf == 0xff:
+        evtType = "Meta"
+        mevtType = chunk.data[cidx] & 0xff
+        cidx += 1
+        mevtLen = chunk.data[cidx] & 0xff
+        cidx += 1
+        cidx +=1 mevtLen
+
+
+
+
+#----------------------------------------------------------------------
 # Chunk Class
+#---
+# A chunk in a MIDI file.  Either a MHdr or MTrk
 #----------------------------------------------------------------------
 class Chunk:
   ctype = None
@@ -17,31 +98,11 @@ class Chunk:
     self.data = data
 
 #----------------------------------------------------------------------
-# SeqHdr Class
+# MidiSeq Class
+#---
+# Header data and parsed track data
 #----------------------------------------------------------------------
-class SeqHdr:
-  fmt = None
-  ntrks = None
-  division = None
-
-  #--------------------------------------------------------------------
-  def __init__(self, chunk):
-    logging.debug("Chunk|"+chunk.ctype)
-    HexDump("Chunk Data", chunk.data)
-
-#----------------------------------------------------------------------
-# SeqTrk Class
-#----------------------------------------------------------------------
-class SeqTrk:
-  #--------------------------------------------------------------------
-  def __init__(self, chunk):
-    logging.debug("Chunk|"+chunk.ctype)
-    HexDump("Chunk Data", chunk.data)
-
-#----------------------------------------------------------------------
-# Sequence Class
-#----------------------------------------------------------------------
-class Sequence:
+class MidiSeq:
   hdr = None
   trks = []
 
@@ -49,14 +110,16 @@ class Sequence:
   def __init__(self, chunks):
     for chunk in chunks:
       if chunk.ctype == "MThd":
-        self.hdr = SeqHdr(chunk)
-      elif chunk.ctpe == "MTrk":
-        self.trks.append(SeqTrk(chunk))
+        self.hdr = MHdr(chunk)
+      elif chunk.ctype == "MTrk":
+        self.trks.append(MTrk(chunk))
       else:
         pass
 
 #----------------------------------------------------------------------
 # MidiFile Class
+#---
+# Reads a MIDI file into memory and parses it into its compenent parts.
 #----------------------------------------------------------------------
 class MidiFile:
   chunks = []
@@ -78,7 +141,6 @@ class MidiFile:
     binFile.readinto(self.data)
     binFile.close()
 
-    #HexDump("File", self.data[0: 4])
     HexDump("File", self.data[0:34])
 
     idx = 0
